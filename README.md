a1814c203f6be67dfdde0bf7fca49e40004c91fd
Forairaaaaa <applesyqd@outlook.com>
2023-02-10 14:12:19 +0800

    6

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LvglTemplate_CM4
-    git clone https://github.com/lvgl/lvgl.git -b  v8.3.5
-    git clone https://github.com/lvgl/lv_drivers.git
+    'git clone https://github.com/lvgl/lvgl.git -b  v8.3.5'
+    'cd lv_porting'
+    'git clone https://github.com/lvgl/lv_drivers.git'
+    'git clone https://github.com/libts/tslib.git'
--- main.cpp
+++ main.cpp
@@ -9,11 +9,15 @@
  * 
  */
 #include <iostream>
+#include <string>
 #include <unistd.h>
 
 #include "lvgl/lvgl.h"
 #include "lvgl/demos/lv_demos.h"
 #include "lv_porting/lv_port_disp.h"
+
+
+
 
 
 int main(int argc, char const *argv[])
@@ -27,16 +31,27 @@ int main(int argc, char const *argv[])
 
 
     // lv_demo_widgets();
-    // lv_demo_stress();
+    lv_demo_stress();
     // lv_demo_benchmark();
-    lv_demo_music();
+    // lv_demo_music();
+
+
+
+
+    
+
+
+
+
 
 
 
     while(1)
     {
         lv_timer_handler();
-        usleep(5 * 1000);
+        usleep(5000);
     }
     return 0;
 }
+
+
